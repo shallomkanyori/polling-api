41d3c6b3ef23449a16c1209dc0908b6cf2850bd2
shallomkanyori <shallomkanyori@gmail.com>
2025-03-09 12:47:57 +0300

    Update build_files script using venv

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,14 @@
+echo "Creating virtual environment..."
+python3.9 -m venv venv
+
+echo "Activating virtual environment..."
+source venv/bin/activate
+
 echo "Installing requirements..."
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Migrating database..."
-python3.9 manage.py migrate --noinput
+python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
