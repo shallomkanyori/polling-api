b40f6e82f449bbd2423eea691a96cad9bb472d43
shallomkanyori <shallomkanyori@gmail.com>
2025-03-10 09:20:59 +0300

    Properly implement signup route

--- polling_api/views.py
+++ polling_api/views.py
@@ -195,8 +195,11 @@ def signup(request):
     """
     API endpoint for user signup
     """
-    userviewset = UserViewSet()
-    return userviewset.create(request)
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @swagger_auto_schema(
     method='post',
